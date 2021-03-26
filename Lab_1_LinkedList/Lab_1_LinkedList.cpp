f9da7dd240816dc84dbb93a0547a3af7d0128861
Алеся Драконова <drakonova02@gmail.com>
2021-03-26 17:47:44 +0200

    Create metods for class: isFull(), isEmpty(), printList(), listsize(), addItem()

--- Lab_1_LinkedList/Lab_1_LinkedList.cpp
+++ Lab_1_LinkedList/Lab_1_LinkedList.cpp
@@ -18,6 +18,11 @@ class List {
 public:    // methods of the List ADT
 	List(int = 10);	//default constructor
 	~List();	//destructor
+	bool isFull();
+	bool isEmpty();
+	int listSize();
+	void printList();
+	void addItem(T item);
 private:     // data members for linked list implementation
 	int count;
 	int max; //max number of nodes
@@ -47,10 +52,71 @@ List<T>::~List() {
 	makeEmpty();
 }//~List
 
+template <typename T>
+bool List<T>::isEmpty() {
+	return count == 0; // return first == NULL;	}//empty
+}
+
+template <typename T>
+int List<T>::listSize() {
+	return count;
+}//listSize
+
+template <typename T>
+bool List<T>::isFull() {
+	bool result = false;
+	Node<T>* temp;
+	temp = new Node <T>();
+	if (temp == NULL) {
+		delete temp;
+		return true;
+	}
+	else if (max < count + 1) return true;
+	else return false;
+}//isFull
+
+template <typename T>
+void List<T>::addItem(T item) {
+	if (isFull()) {
+		cout << "List overflow\n";
+		return; //return to the calling function
+	}
+	else {
+		Node<T>* previous = NULL, * following = first;
+		while (following != NULL && following->data < item)
+		{
+			previous = following;
+			following = following->next;
+		}//while
+		if (previous == NULL) first = new Node<T>(item, first);
+		else previous->next = new Node<T>(item, following);
+		count += 1;
+	}
+}//addItem
+
+template <typename T>
+void List<T>::printList() {
+	cout << "\n\nList content:\n";
+	if (count == 0) cout << "list is empty.\n";
+	Node<T>* p = first;
+	while (p != NULL) {
+		cout << p->data << ", ";
+		p = p->next;
+	}//while
+	cout << endl;
+}//printList
+
+
 void main()
 {
 	int a;
 	cout << "Write max number for nodes: ";
 	cin >> a;
 	List <string> A(a); //object A of class List with int items Note
+	A.addItem("bet");
+	A.addItem("ann");
+	A.addItem("vanda");
+	A.addItem("sara");
+	A.addItem("archi");
+	A.printList();
 }
