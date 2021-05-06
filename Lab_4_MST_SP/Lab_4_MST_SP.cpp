4b348300ae785e8d5c6a69c6099c9277f58f5603
Алеся Драконова <drakonova02@gmail.com>
2021-05-06 17:56:12 +0300

    Create macket for constructor

--- Lab_4_MST_SP/Lab_4_MST_SP.cpp
+++ Lab_4_MST_SP/Lab_4_MST_SP.cpp
@@ -9,6 +9,12 @@ struct edge{
 	int weight;
 	edge* next; //link member
 
+	edge() {
+		vertex1 = 0;
+		vertex2 = 0;
+		weight = 0;
+		next = NULL;
+	}
 	edge(int v1, int v2, int w) //constructor
 	{
 		vertex1 = v1;
@@ -18,8 +24,11 @@ struct edge{
 	}
 };
 
-class Graph
-{
+typedef edge* p_edge;
+
+//A Graph object is created via dialog with a user.
+//The object is represented by linked adjacency lists unsorted by numbers of vertices.
+class Graph{
 public:
 	Graph();
 	~Graph();
@@ -29,9 +38,61 @@ public:
 private:
 	int n; // number of vertices
 	int m; // number of edges in graph or digraph
-	edge* adjacencyList; //pointer to array of pointers to adjacency lists
+	p_edge* adjacencyList; //pointer to array of pointers to adjacency lists
 	void Print_list(int);
 };
+
+//Constructor
+
+Graph::Graph(){ 
+	int edge_weight; //to read edge weights
+	int answer; //to read numbers of adjacent vertices
+
+	cout << "Enter number of graph vertices ==> ";
+	cin >> n;
+
+	//A dynamic array of pointers to linked adjacency lists is created and initialized.
+	adjacencyList = new p_edge[n + 1, NULL];
+	for (int i = 1; i <= n; i++) adjacencyList[i] = NULL;
+
+	int edge_count = 0;
+	for (int i = 1; i <= n; i++) //Adjacency linked lists for an input graph are created.
+	{
+		cout << " \nADJACENCY LIST for graph VERTEX #" << i << endl;
+		answer = 1;
+		while (answer != -1)
+		{
+			cout << "Enter number of current adjacent vertex (-1 to finish) ==> ";
+			cin >> answer;
+			if (answer == -1) break;
+			if (answer >= 1 && answer <= n)
+			{
+				cout << "Enter integer weight of edge " << i << "--" << answer << " ==> ";
+				cin >> edge_weight;
+				/*if(check node) contiue*/
+				//insert new node in linked list of i
+				//insert new node in linked list of answer
+				edge_count++;
+			}
+			else cout << "VERTEX NUMBER IS OUT OF RANGE!!\n";
+		}
+	}
+	m = edge_count;
+	cout << "\nADJACENCY LISTS for each vertex of input graph:\n";
+	for (int i = 1; i <= n; i++)
+	{
+		cout << "list for vertex #" << i << ": ";
+		//Print_list(i);
+	}
+}// constructor
+
+//Destructor
+
+
+
+// Metods of class
+
+
 
 int main()
 {
