0bec8686e8c226afc06a0783885297bf9df8583a
Алеся Драконова <drakonova02@gmail.com>
2021-05-28 04:28:30 +0300

    Make metod add for random keys

--- Lab_5_Hash_Table/Lab_5_Hash_Table.cpp
+++ Lab_5_Hash_Table/Lab_5_Hash_Table.cpp
@@ -21,20 +21,23 @@ private:
     int primeTableSize;
     int collis;
     NodePtr* table; //pointer to a dynamic array of pointers
+    int count;
 
     int h(int k) { return k % primeTableSize;};     //division hash function
-    //void AddKeys();
+    void AddKeys();
 
 }; //HashTable
 
-HashTable::HashTable(int tableSize) {
+HashTable::HashTable(int n) {
 
     primeTableSize = 101;
+    collis = 0;
+    count = n;
     table = new NodePtr[primeTableSize];
 
     for (int j = 0; j < primeTableSize; j++) table[j] = NULL;
 
-    //AddKeys();
+    AddKeys();
 
 }//constructor
 
@@ -58,6 +61,17 @@ HashTable::~HashTable() {
     }
 }//destructor
 
+void HashTable::AddKeys() {
+    for (int i = 1; i != count + 1; ++i) {
+
+        int k = rand() % 449 + 1;   //random item
+        int index = h(k); // index is the hash address for item .
+        NodePtr a = new NodeType;
+        a->key = k;
+        a->next = table[index];
+        table[index] = a;
+    }
+}//insert
 
 
 int main()
