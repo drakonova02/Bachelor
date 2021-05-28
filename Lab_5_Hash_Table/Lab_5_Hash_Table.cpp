5bcfa2102a0e5ceef3d0aed5d0e44b77a489aa58
Алеся Драконова <drakonova02@gmail.com>
2021-05-28 04:09:48 +0300

    Add constructor for Hash Table and metod for number of collision

--- Lab_5_Hash_Table/Lab_5_Hash_Table.cpp
+++ Lab_5_Hash_Table/Lab_5_Hash_Table.cpp
@@ -15,12 +15,26 @@ class HashTable {
 public:
     HashTable(int);
     ~HashTable();
+    int Collision() { return collis; };
 
 private:
     int primeTableSize;
+    int collis;
     NodePtr* table; //pointer to a dynamic array of pointers
+    //void AddKeys();
 
 }; //HashTable
+
+HashTable::HashTable(int tableSize) {
+
+    primeTableSize = 101;
+    table = new NodePtr[primeTableSize];
+
+    for (int j = 0; j < primeTableSize; j++) table[j] = NULL;
+
+    //AddKeys();
+
+}//constructor
 
 
 int main()
